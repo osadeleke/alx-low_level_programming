f5e1c5752466c6a2333582d4dd6292649acfa706
Olusegun Adeleke <adesegs97@gmail.com>
2023-03-16 21:08:10 +0000

    add task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Prints characters with printf
+ *
+ * return: 0 if successful
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
