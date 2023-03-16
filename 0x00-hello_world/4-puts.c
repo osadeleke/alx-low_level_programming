804a7b1eec54667d902a4008799db44b7c1a320c
Olusegun Adeleke <adesegs97@gmail.com>
2023-03-16 21:06:02 +0000

    edit task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
 /**
+ * main - Prints a string of char.
  *
+ * Return: 0 if successful
  */
 
-int main void()
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
 }
