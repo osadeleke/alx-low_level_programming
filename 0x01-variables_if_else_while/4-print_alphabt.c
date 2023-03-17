b6f3c7cfd4cb706ad3621d09f7c8bba9480b167d
Olusegun Adeleke <adesegs97@gmail.com>
2023-03-17 14:24:47 +0000

    add task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - print lower case alphas without q and e
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != q && c != e)
+		{
+			putchar(c);
+		}
+		c++;
+	}
+	putchar('\n');
+
+	return (0);
+}
