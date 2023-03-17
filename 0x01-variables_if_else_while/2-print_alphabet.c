e8450bf5b1be837d8c0ce6d1fae1218b9a639b1c
Olusegun Adeleke <adesegs97@gmail.com>
2023-03-17 14:12:14 +0000

    add task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - function prints all lower case alphas using putchar
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c = 'a';
+
+	while(c <= 'z')
+	{
+	putchar(c);
+	putchar('\n');
+	c = c + 1;
+	}
+
+	return (0)
+}	
