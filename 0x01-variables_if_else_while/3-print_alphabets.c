efcca7e3c4c8567d6a4edd8ee635bf84cac9c3ec
Olusegun Adeleke <adesegs97@gmail.com>
2023-03-17 14:20:05 +0000

    add task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints upper and lower case alpha
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char a = 'a';
+	char b = 'A';
+
+	while (a <= 'z')
+	{
+		putchar(a);
+		a++;
+	}
+	while (b <= 'Z')
+	{
+		putchar(b);
+		b++;
+	}
+	putchar('\n');
+
+	return (0);
+}
