ca13b4a064db1e8d0e0f1a786d9644d21e522fce
Olusegun Adeleke <adesegs97@gmail.com>
2023-03-17 14:28:32 +0000

    add task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints all base 10 digits from 0 to 9
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a;
+	
+	for (a = 0; a < 10; a++)
+	{
+		printf("%d", a);
+	}
+
+	return (0);
+}
