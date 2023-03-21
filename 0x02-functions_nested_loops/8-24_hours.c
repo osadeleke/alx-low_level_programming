8d2ea7d03dd18d0ab77390b1815825cf1ba30f06
Olusegun Adeleke <adesegs97@gmail.com>
2023-03-21 16:20:30 +0000

    correct task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,10 +12,10 @@ void jack_bauer(void)
 
 	for (i = 48; i < 51; i++)
 	{
-		for (j = 48; j < 52, j++)
+		for (j = 48; j < 52; j++)
 		{
 			for (k = 48; k < 54; k++)
-			{
+			{`
 				for (l = 48; l < 58; l++)
 				{
 					_putchar(i);
