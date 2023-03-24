a41cceebf3c39de8cd9c880524ad17dfdc474e55
Olusegun Adeleke <adesegs97@gmail.com>
2023-03-24 09:59:08 +0000

    add task 5 to 9

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_diagonal - function that prints forward slashes diagonally
+ * @n: integer
+ *
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int i = 1, j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+
+	while (i <= n)
+	{
+		for (j = 0; j < 1; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+		i++;
+	}
+}
