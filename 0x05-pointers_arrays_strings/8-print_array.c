3221cb1833efda748fb231a864c18b7c013dc4f8
Olusegun Adeleke <adesegs97@gmail.com>
2023-03-27 16:00:12 +0000

    correct task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,13 +13,13 @@ void print_array(int *a, int n)
 
 	for (i = 0; i <= n; i++)
 	{
-		if (i < n)
+		if (i < (n - 1))
 		{
 			printf("%d, ", *(a + i));
 		}
-		else if (i == n)
+		else if (i == n - 1)
 		{
-			printf("%d", *(a + i));
+			printf("%d", *(a + (n - 1)));
 		}
 	}
 	printf("\n");
