5598ae337a35e1737d0ece0037d39611a1c7cb57
Olusegun Adeleke <adesegs97@gmail.com>
2023-03-27 11:57:22 +0000

    add task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,5 +9,5 @@
 
 void print_array(int *a, int n)
 {
-	printf("%d, %d, %d, %d, %d\n", *a, *(a + (n - 4)), *(a + (n - 3)), *(a + (n - 2)), *(a + (n - 1));
+	printf("%d, %d, %d, %d, %d\n", *a, *(a + (n - 4)), *(a + (n - 3)), *(a + (n - 2)), *(a + (n - 1)));
 }
