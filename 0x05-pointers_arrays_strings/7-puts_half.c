a1f71177c63f85965bab17268668ce1f0ae3c5cc
Olusegun Adeleke <adesegs97@gmail.com>
2023-03-27 11:48:42 +0000

    add task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -18,14 +18,14 @@ void puts_half(char *str)
 
 	if (i % 2 != 0)
 	{
-		for (n = (i + 1) / 2; n <= i; n++)
+		for (n = (i + 1) / 2; n < i; n++)
 		{
 			_putchar(str[n]);
 		}
 	}
 	else
 	{
-		for (n = i / 2; n <= i; n++)
+		for (n = i / 2; n < i; n++)
 		{
 			_putchar(str[n]);
 		}
