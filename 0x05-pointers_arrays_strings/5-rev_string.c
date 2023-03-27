501faadc1f7ab83af6f977a44dc19b6aa0a0ed75
Olusegun Adeleke <adesegs97@gmail.com>
2023-03-27 11:04:10 +0000

    add task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: string to be reversed.
+ */
+
+void rev_string(char *s)
+{
+	int i, j, n;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	n = i - 1;
+	j = 0;
+	while (n >= 0)
+	{
+		s[n] = _putchar(s[i - j]);
+		n--;
+		j++;
+	}
+}
