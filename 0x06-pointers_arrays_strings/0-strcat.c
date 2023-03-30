5ba992700fb12ca1da254206d967f4fc6848f392
Olusegun Adeleke <adesegs97@gmail.com>
2023-03-30 13:15:54 +0000

    add task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenate two strings.
+ * @dest: first string
+ * @src: second string
+ *
+ * Return: Dest string.
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j, k, l;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	j = 0;
+	while (src[j] != '\0')
+	{
+		j++;
+	}
+
+	l = 0;
+	for (k = i; k >= i && k < j + 1; k++)
+	{
+		dest[k] = src[l];
+		l++;
+	}
+
+	return (dest);
+}
