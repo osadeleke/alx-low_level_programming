97ce3ec080c6ae102dbec2de7127e44cf2d964b5
Olusegun Adeleke <adesegs97@gmail.com>
2023-03-30 17:07:30 +0000

    update main.h with all task prototype

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,5 +2,12 @@
 #define MAIN_H
 
 char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _stcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+int _putchar(char c);
 
 #endif
