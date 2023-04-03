e426d1d4331c860926751855aa2721edf902d816
Olusegun Adeleke <adesegs97@gmail.com>
2023-04-03 17:10:38 +0000

    add task 2 again

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in string]
+ * @s: string
+ * @c: character to be located
+ *
+ * Return: string after character
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+
+	return (s + 1);
+}
