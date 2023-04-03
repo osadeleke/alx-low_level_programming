0cca95b5315089f53f7eaa194b49d2384da42597
Olusegun Adeleke <adesegs97@gmail.com>
2023-04-03 14:23:31 +0000

    add task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr - function that locates a character in a string
+ * @s: string
+ * @c: character to be located
+ *
+ * Return: pointer to first occurence if found or else NULL
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return s;
+		}
+		s++
+	}
+	return NULL;
+}
