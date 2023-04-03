bbc69751b1e94528bfd4cceb3f7059c4a3c37f53
Olusegun Adeleke <adesegs97@gmail.com>
2023-04-03 13:06:29 +0000

    add task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memcpy - function to copy memory area
+ * @dest: memory area to copy from
+ * @src: memory area to copy to
+ *
+ * Return: Dest if successful
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
