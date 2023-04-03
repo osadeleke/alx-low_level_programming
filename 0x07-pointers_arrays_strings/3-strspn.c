81dcd6647385399297d4ed6badfbd1e74d146ad1
Olusegun Adeleke <adesegs97@gmail.com>
2023-04-03 15:31:54 +0000

    correct task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -13,18 +13,25 @@ unsigned int _strspn(char *s, char *accept)
 	unsigned int i, j, k;
 
 	i = j = k = 0;
-
-	while (s[j] != ',')
+	
+	if (s == NULL)
 	{
-		for (; accept[i] != '\0'; i++)
-		{
-			if (s[j] == accept[i])
-			{
-				k++;
-			}
-		}
-		j++;
+		return (k);
 	}
+	else
+	{
+		while (s[j] != ',')
+		{
+			for (; accept[i] != '\0'; i++)
+			{
+				if (s[j] == accept[i])
+				{
+					k++;
+				}
+			}
+			j++;
+		}
 
-	return (k);
+		return (k);
+	}
 }
