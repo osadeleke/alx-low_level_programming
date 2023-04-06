6685f98d1244a4bb64a06e02aea684f558ac7f2d
Olusegun Adeleke <adesegs97@gmail.com>
2023-04-06 11:52:19 +0000

    add task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string
+ *
+ * Return: lenght of string
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+	else
+		return (0);
+}
