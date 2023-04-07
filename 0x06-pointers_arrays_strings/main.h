e50017b5a5024af12b6c63225b91ba80a30c974c
Olusegun Adeleke <adesegs97@gmail.com>
2023-04-07 17:16:06 +0000

    add task 5

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -6,7 +6,7 @@ char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _stcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
-char *string_toupper(char *);
+char *string_toupper(char *str);
 char *cap_string(char *);
 int _putchar(char c);
 
