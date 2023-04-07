3e79aff90e5d61ef06f0ef6995a1dd54286e26d0
Olusegun Adeleke <adesegs97@gmail.com>
2023-04-07 15:54:05 +0000

    correct task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,20 +13,17 @@ char *_strncpy(char *dest, char *src, int n)
 	char *p = dest;
 	int i;
 
-	if (*src == '\0')
+	for (i = 0; i < n; i++)
 	{
-		for (i = 0; i < n; i++)
-		{
-			*p = '\0';
-			p++;
-		}
-	}
-
-	else {
-		for (i = 0; i < n; i++)
+		if (*src != '\0')
 		{
 			*p = *src;
 			src++;
+			p++;
+		}
+		else
+		{
+			*p = '\0';
 			p++;
 		}
 	}
