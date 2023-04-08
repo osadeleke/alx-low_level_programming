29f7e459f84af9a433dfab5131bb57e68f4dc598
Olusegun Adeleke <adesegs97@gmail.com>
2023-04-08 02:20:57 +0000

    add task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - program multiplies two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 if successful and 1 otherwise
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc < 2 || argc > 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		printf("%d\n", argv[1] * argv[2]);
+	}
+
+	return (0);
+}
