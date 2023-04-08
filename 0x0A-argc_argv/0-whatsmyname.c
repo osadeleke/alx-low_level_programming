4c9e650a2cbc6670d1b9e8fe8f804902572fa2b2
Olusegun Adeleke <adesegs97@gmail.com>
2023-04-08 01:58:09 +0000

    add task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints program name followed by new line
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 if successful
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
