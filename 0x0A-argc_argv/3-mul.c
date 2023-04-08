aca123ecd66a33ec15ce0d1ad86e62a82aabbeb8
Olusegun Adeleke <adesegs97@gmail.com>
2023-04-08 02:27:46 +0000

    correct task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - program multiplies two numbers
@@ -10,14 +11,14 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc < 2 || argc > 2)
+	if (argc < 3 || argc > 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	else (if argc == 2)
+	else if (argc == 3)
 	{
-		printf("%d\n", (argv[1] * argv[2]));
+		printf("%d\n", (atoi(argv[1]) * atoi(argv[2])));
 	}
 
 	return (0);
