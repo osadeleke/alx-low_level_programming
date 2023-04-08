14e2e57d0b43f0bd7319be656f42b36250d8fdb0
Olusegun Adeleke <adesegs97@gmail.com>
2023-04-08 02:10:52 +0000

    add task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints number of arguments passed into it.
+ * @argc: number of arguments passed.
+ * @argv: string of arguments
+ *
+ * Return: 0 if successful
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	printf("%d\n", arg - 1);
+	if (i > argc)
+	{
+		printf("%s", argv[i]);
+	}
+	return (0);
+}
