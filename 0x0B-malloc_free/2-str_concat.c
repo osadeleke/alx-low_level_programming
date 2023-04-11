fc98e341349ebcabba4deb17e8fc20026cab36ef
Olusegun Adeleke <oadeleke123@gmail.com>
2023-04-11 21:02:07 +0000

    add task 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - function that concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: NULL if zero and pointer to concatenated string otherwise
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+
+}
