077a5f1e986a7b1d394641af7b6884b122a74a18
Olusegun Adeleke <oadeleke123@gmail.com>
2023-04-12 10:06:25 +0000

    add task 3 and 4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - function that frees a 2 dimensional grid.
+ * @grid: double pointer to grid
+ * @height: the hieght of the grid
+ *
+ * Return: void
+ */
+
+
