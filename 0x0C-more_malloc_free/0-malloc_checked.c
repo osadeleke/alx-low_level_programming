b87df08310805bc67654f5ca422e6ebce6eb691a
Olusegun Adeleke <oadeleke123@gmail.com>
2023-04-13 10:27:17 +0000

    add task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - function allocates memory using malloc
+ * @b: bytes
+ * 
+ * Return: 98 if failed. pointer otherwise.
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *p;
+
+	p = malloc(b);
+	if (p == NULL)
+	{
+		exit(98);
+	}
+	else
+		return (p);
+}
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void *malloc_checked(unsigned int b);
+
+
+#endif
