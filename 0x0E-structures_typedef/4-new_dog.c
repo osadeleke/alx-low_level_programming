a098d2e3cdd43f7defc22be4cd7fbc03ce0bcbb2
Olusegun Adeleke <oadeleke123@gmail.com>
2023-04-17 18:16:12 +0000

    correct task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,9 +16,13 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
+	{
 		return (NULL);
+	}
 
 	(*new_dog).name = name;
 	(*new_dog).age = age;
 	(*new_dog).owner = owner;
+	
+	return (new_dog);
 }
