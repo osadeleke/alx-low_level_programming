fe92aab26ee03691755e07faa8af1b838435c470
Olusegun Adeleke <oadeleke123@gmail.com>
2023-04-17 21:36:30 +0000

    correct task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -20,9 +20,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	(*new_dog).name = name;
-	(*new_dog).age = age;
-	(*new_dog).owner = owner;
+	new_dog->name = name;
+	new_dog->age = age;
+	new_dog->owner = owner;
 	
 	return (new_dog);
 }
