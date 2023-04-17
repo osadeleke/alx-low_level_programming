4a1801fbd9dd0ee8a77fc43588a412f50b6859bf
Olusegun Adeleke <oadeleke123@gmail.com>
2023-04-17 17:59:44 +0000

    correct task 5

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,5 +18,6 @@ struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 typedef struct dog dog_t;
+void free_dog(dog_t *d);
 
 #endif /* DOG_H */
