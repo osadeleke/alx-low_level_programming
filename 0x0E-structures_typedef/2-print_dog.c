b530d7a650ea846eb3acd1baabeafeeafae02ec0
Olusegun Adeleke <oadeleke123@gmail.com>
2023-04-17 21:23:50 +0000

    correct task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -17,7 +17,7 @@ void print_dog(struct dog *d)
 		else
 			printf("Name: %s\n", (*d).name);
 
-		if (!(*d).age)
+		if ((*d).age < 0)
 			printf("Age: (nil)\n");
 		else
 			printf("Age: %.1f\n", (*d).age);
