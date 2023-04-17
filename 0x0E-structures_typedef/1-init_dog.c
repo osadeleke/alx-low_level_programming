9bfaebdfb53aa19a67e5e3582a81b77390f1f8b1
Olusegun Adeleke <oadeleke123@gmail.com>
2023-04-17 17:18:06 +0000

    add task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+
+/**
+ * init_dog - initialize dog function
+ * @d: dog function
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * Return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
