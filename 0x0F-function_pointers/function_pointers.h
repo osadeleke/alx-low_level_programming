8c9a6b54ed97d97005ecbf251d311014ae8d4159
Olusegun Adeleke <oadeleke123@gmail.com>
2023-04-19 15:12:47 +0000

    correct task 1

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -3,7 +3,7 @@
 #define FUNCTION_POINTERS_H
 int _putchar(char c);
 void print_name(char *name, void(*f)(char *));
-void array_iterator(int *array, size_t, void(*action)(int));
+void array_iterator(int *array, size_t size, void(*action)(int));
 
 
 #endif /* FUNCTION_POINTERS_H */
