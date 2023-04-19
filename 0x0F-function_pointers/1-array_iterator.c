e61d713f286cb66626774557996b4900dd56cd61
Olusegun Adeleke <oadeleke123@gmail.com>
2023-04-19 15:08:37 +0000

    add task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - iterates an array.
+ * @array: array to be iterated.
+ * @size: size of the array
+ * @action: pointer to the function to use.
+ *
+ * Return: void.
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		(*action)(array[i]);
+	}
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -3,6 +3,7 @@
 #define FUNCTION_POINTERS_H
 int _putchar(char c);
 void print_name(char *name, void(*f)(char *));
+void array_iterator(int *array, size_t, void(*action)(int));
 
 
 #endif /* FUNCTION_POINTERS_H */
