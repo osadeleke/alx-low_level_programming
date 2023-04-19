bb45387663a196819a76f83271ee8372af8a7827
Olusegun Adeleke <oadeleke123@gmail.com>
2023-04-19 21:35:19 +0000

    correct task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,15 +13,15 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	size_t int i;
-
 	if (array == NULL || action == NULL)
 	{
 		return;
 	}
 
-	for (i = 0; i < size; i++)
+	while (size > 0)
 	{
-		action(array[i]);
+		action(*array);
+		array++;
+		size--;
 	}
 }
