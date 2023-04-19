615749e5a10bcc54e0963804e475d8853989fe28
Olusegun Adeleke <oadeleke123@gmail.com>
2023-04-19 14:42:06 +0000

    add task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - function to pointer to print name
+ * @name: name string.
+ * @f: pointer to function.
+ *
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
