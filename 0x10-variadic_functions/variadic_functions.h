c499379b80437a66755bbab9ece48ea1b9cc19c2
Olusegun Adeleke <oadeleke123@gmail.com>
2023-04-20 18:28:43 +0000

    correct task 1

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -7,7 +7,7 @@
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
-void print_number(const char *separator, const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
 
 
 #endif /* Variadic_functions */
