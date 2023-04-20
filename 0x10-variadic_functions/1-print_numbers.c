165b6f923932281ac54ad46cf300472125e0168e
Olusegun Adeleke <oadeleke123@gmail.com>
2023-04-20 18:46:21 +0000

    correct task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -18,7 +18,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(arg, int));
-		if (!(separator == NULL))
+		if (separator != NULL && i != n - 1)
 		{
 			printf("%s", separator);
 		}
