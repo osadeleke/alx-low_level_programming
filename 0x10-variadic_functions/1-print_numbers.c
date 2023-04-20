c5b6903917bf25ae7d7f7a46a5d3de540be0b1a8
Olusegun Adeleke <oadeleke123@gmail.com>
2023-04-20 18:30:43 +0000

    correct task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -15,7 +15,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(arg, n);
 
-	if (separator == NULL || n == 0)
+	if (separator == NULL)
 		return;
 
 	for (i = 0; i < n; i++)
