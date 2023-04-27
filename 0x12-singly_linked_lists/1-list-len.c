8d488a27dfa95b45b6462d00ab8b2a0d76f6330e
Olusegun Adeleke <oadeleke123@gmail.com>
2023-04-27 23:30:59 +0000

    add task 1

---
+++ 0x12-singly_linked_lists/1-list-len.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * print_list - prints elements of a list.
+ * @h: pointer to the head of node.
+ *
+ * Return: size of elements in list.
+ */
+
+size_t list_len(const list_t *h)
+{
+	int n;
+
+	if (h == NULL)
+		return (-1);
+
+	n = 1;
+	if (h->next == NULL)
+	{
+		n++;
+		return (n);
+	}
+	else if (h->next != NULL)
+	{
+		n++;
+		h = h->next;
+		list_len(h);
+	}
+	return (n);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -26,5 +26,6 @@ typedef struct list_s
 /* function prototypes */
 int _putchar(char c);
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
 
 #endif /* MAIN_H */
