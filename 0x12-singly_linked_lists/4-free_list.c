65f6751f178388d3ef43626f4355d76ff7c0c978
Olusegun Adeleke <oadeleke123@gmail.com>
2023-04-28 00:16:11 +0000

    add task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_list - free a list.
+ * @head: pointer to the head of node.
+ */
+
+void free_list(list_t *head)
+{
+	list_t *new;
+	list_t *ran;
+
+	new = head;
+
+	while (new != NULL)
+	{
+		ran = new->next;
+		free(new->str);
+		free(new);
+		free = ran;
+	}
+}
