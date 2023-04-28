d71abd95fdce221a99fb51c241d978aa84373302
Olusegun Adeleke <oadeleke123@gmail.com>
2023-04-28 00:22:10 +0000

    add task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * con - prints before executing main
+ */
+
+void con(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
