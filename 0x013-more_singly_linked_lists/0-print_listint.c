b867f58ffd9d05edbc3fefc8fe64b3c79159ab1b
Olusegun Adeleke <oadeleke123@gmail.com>
2023-04-30 05:55:20 +0000

    add task 0

---
+++ 0x013-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints elements of list
+ * @h: pointer to head of list.
+ *
+ * Return: n the number of elements in list
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	int n = 0;
+	
+	while (h)
+	{
+		printf("%d\n", h->n);
+		n++;
+		h = h->next;
+	}
+
+	return (n);
+}
---
+++ 0x013-more_singly_linked_lists/lists.h
@@ -0,0 +1,28 @@
+#ifndef LIST_H
+#define LIST_H
+
+/* headers */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* structures */
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the the next node
+ *
+ * Description: singly linked list node structure
+ *
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+/* function prototypes */
+int _putchar(char c);
+size_t print_listint(const listint_t *h);
+
+#endif
