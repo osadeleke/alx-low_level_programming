0863446c02bed5517b3f2b8aa06aa000ca58b1a3
Olusegun Adeleke <oadeleke123@gmail.com>
2023-05-02 17:51:43 +0000

    correct task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -32,7 +32,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	for (i = 0; i < idx - 1; i++)
 	{
 		if (p == NULL)
-			return;
+			return (NULL);
 		p = p->next;
 	}
 
