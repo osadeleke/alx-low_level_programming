385c30fe7491644fae83e989eef08b31e06a5c6f
Olusegun Adeleke <oadeleke123@gmail.com>
2023-05-06 13:15:34 +0000

    add task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: number in decimal
+ *
+ * Return: void
+ */
+
+void print_binary(unsigned long int n)
+{
+	int check = 1, w = n, i = 0;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	while (w > 0)
+	{
+		i++;
+		w = w >> 1;
+	}
+
+	check = check << (i - 1);
+
+	for (; i > 0; i--)
+	{
+		if (n & check)
+			_putchar('1');
+		else
+			_putchar('0');
+
+		check = check >> 1;
+	}
+}
