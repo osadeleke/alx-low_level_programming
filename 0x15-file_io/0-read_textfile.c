f75fde1036a454e66fc1caf46df4b386cde049eb
Olusegun Adeleke <oadeleke123@gmail.com>
2023-05-08 16:21:47 +0000

    add task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX.
+ * @filename: a pointer to the file to be read.
+ * @letters: number of letters it should read and print
+ *
+ * Return; number of letters it read and printed otherwise 0
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char *b;
+	size_t r, w;
+
+	if (filename == NULL)
+		return (0);
+
+	b = malloc(sizeof(char) * letters);
+	if (b == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	r = read(fd, b, letters);
+	w = write(STDOUT_FILENO, b, r);
+
+	close(fd);
+
+	free(b);
+
+	return (w);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Headers */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+/* Function Prototypes */
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif /** MAIN_H **/
