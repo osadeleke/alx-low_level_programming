7c2a9ea92dd5197fce9a69f39e87290f4f318164
Olusegun Adeleke <oadeleke123@gmail.com>
2023-05-08 17:48:12 +0000

    corrected main.h file"

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,6 +10,6 @@
 /* Function Prototypes */
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
-int creat_file(const char *filename, char *text_content);
+int create_file(const char *filename, char *text_content);
 
 #endif /** MAIN_H **/
