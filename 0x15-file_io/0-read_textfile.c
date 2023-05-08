2088b3d05c96f7320354452b9fd083516c1cc995
Olusegun Adeleke <oadeleke123@gmail.com>
2023-05-08 16:25:02 +0000

    add return 0 if file cannot be opened or read

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -22,6 +22,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
 
 	r = read(fd, b, letters);
 	w = write(STDOUT_FILENO, b, r);
