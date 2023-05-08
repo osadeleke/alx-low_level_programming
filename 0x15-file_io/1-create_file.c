52c7dc8b3e6ec672ce75dd4b7cab015bba42406a
Olusegun Adeleke <oadeleke123@gmail.com>
2023-05-08 23:33:04 +0000

    correct task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,7 +10,7 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, length;
+	int fd, length, w;
 
 	if (filename == NULL)
 		return (-1);
@@ -20,10 +20,12 @@ int create_file(const char *filename, char *text_content)
 
 	/** open file **/
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-
+	if (fd == -1)
+		return (-1);
 	/** process open file **/
-	write(fd, text_content, length);
-
+	w = write(fd, text_content, length);
+	if (w == -1)
+		return (-1);
 	/** close file **/
 	close(fd);
 
