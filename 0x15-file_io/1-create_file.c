d2b262d495861a1749e2186d24a3473154e9b16b
Olusegun Adeleke <oadeleke123@gmail.com>
2023-05-08 17:53:04 +0000

    add truncate flag to task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 		length++;
 
 	/** open file **/
-	fd = open(filename, O_CREAT | O_WRONLY, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	/** process open file **/
 	write(fd, text_content, length);
