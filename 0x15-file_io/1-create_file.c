b32c9181ae85572fe202dc93c448dd5cba3ad1e0
Olusegun Adeleke <oadeleke123@gmail.com>
2023-05-09 17:23:31 +0000

    test correction on task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,6 +15,9 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
+	if (text_content == NULL)
+		text_content = "";
+
 	while (text_content[length])
 		length++;
 
