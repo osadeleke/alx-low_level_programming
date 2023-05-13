42f233a831d29d1cc5e22c843671de1d9a06dd30
Olusegun Adeleke <oadeleke123@gmail.com>
2023-05-13 15:53:06 +0000

    add task 3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,80 @@
+#include "main.h"
+
+char *file_from(char *arg1, char *ran)
+{
+	int ff, cf;
+	ssize_t r;
+
+	ff = open(arg1, O_RDONLY);
+	if (ff == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s at open point\n", arg1);
+		exit(98);
+	}
+
+	r = read(ff, ran, 1024);
+	if (r == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s at read point\n", arg1);
+		exit(98);
+	}
+
+	cf = close(ff);
+	if (cf == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", ff);
+		exit(100);
+	}
+
+	return (ran);
+}
+
+/**
+ * main - copy content of a file to another file
+ * @argc: number of arguments
+ * @argv: pointer to an array of the arguments
+ *
+ * Return: 1 if successful, -1 otherwise
+ */
+
+int main(int argc, char **argv)
+{
+	int ft, ct, count;
+	ssize_t w;
+	char *reading;
+	char ran[1024];
+
+	if (argc != 3)
+	{
+		dprintf(2, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	reading = file_from(argv[1], ran);
+
+	ft = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (ft == -1)
+	{
+		dprintf(2, "Error: Can't write to %s at open\n", argv[2]);
+		exit(99);
+	}
+
+	while(reading[count])
+		count++;
+
+	w = write(ft, reading, count);
+	if (w == -1)
+	{
+		dprintf(2, "Error: Can't write to %s at writing\n", argv[2]);
+		exit(99);
+	}
+
+	ct = close(ft);
+	if (ct == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", ft);
+		exit(100);
+	}
+
+	return (1);
+}
