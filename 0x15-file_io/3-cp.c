a69c76d55f4f14779b61ef16f7bd6b72d8ec7b8a
Olusegun Adeleke <oadeleke123@gmail.com>
2023-05-13 22:17:51 +0000

    test new algo

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,7 +16,7 @@ char *file_from(char *arg1, char *ran)
 	ff = open(arg1, O_RDONLY);
 	if (ff == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", arg1);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", arg1);
 		exit(98);
 	}
 	while ((r = read(ff, ran + tb, 1024)) > 0)
@@ -26,13 +26,13 @@ char *file_from(char *arg1, char *ran)
 	if (r == -1)
 	{
 		close(ff);
-		dprintf(2, "Error: Can't read from file %s\n", arg1);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", arg1);
 		exit(98);
 	}
 	cf = close(ff);
 	if (cf == -1)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", ff);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", ff);
 		exit(100);
 	}
 	return (ran);
@@ -55,14 +55,14 @@ int main(int argc, char **argv)
 
 	if (argc != 3)
 	{
-		dprintf(2, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	reading = file_from(argv[1], ran);
 	ft = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (ft == -1)
 	{
-		dprintf(2, "Error: Can't write to %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 	while (reading[count])
@@ -72,13 +72,13 @@ int main(int argc, char **argv)
 	if (w == -1)
 	{
 		close(ft);
-		dprintf(2, "Error: Can't write to %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 	ct = close(ft);
 	if (ct == -1)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", ft);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", ft);
 		exit(100);
 	}
 	return (1);
