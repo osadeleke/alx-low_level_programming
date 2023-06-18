fb7ba4d6c17ff6964e2d3ceb8ddaef8fcbf51ba7
Olusegun Adeleke <oadeleke123@gmail.com>
2023-06-18 01:38:55 +0000

    program returns the nth node of a linked list

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - get nth node of a linked list
+ * @head: pointer to head of list
+ * @index: nth node check
+ * Return: value at nth node
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *current;
+	unsigned int i;
+
+	current = head;
+	for (i = 0; i <= index; i++)
+	{
+		if (!current)
+		{
+			return (NULL);
+		}
+		current = current->next;
+	}
+	return (current);
+}
