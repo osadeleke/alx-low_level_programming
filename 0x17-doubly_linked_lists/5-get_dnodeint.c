16406bea04dcb202c5fa71956ca56cfdc12598d6
Olusegun Adeleke <oadeleke123@gmail.com>
2023-06-18 01:44:34 +0000

    try algorithm to print first node

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -22,7 +22,10 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 		{
 			return (current);
 		}
-		current = current->next;
+		if (i > 0)
+		{
+			current = current->next;
+		}
 	}
 	return (current);
 }
