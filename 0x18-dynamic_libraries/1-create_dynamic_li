0bf5444420dfed0adc9edabf01d423604579b6df
Olusegun Adeleke <oadeleke123@gmail.com>
2023-06-23 06:02:15 +0000

    fix dynamic lib error

---
+++ 0x18-dynamic_libraries/1-create_dynamic_li
@@ -0,0 +1,2 @@
+#!/bin/bash
+gcc -shared -fPIC *.c -o liball.so
--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,6 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdlib.h>
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
