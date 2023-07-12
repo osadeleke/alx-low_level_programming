a2a2c96a480d7341c6fbe71d533e69237bd3a5ef
Olusegun Adeleke <oadeleke123@gmail.com>
2023-07-12 16:48:48 +0000

    fix: hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,17 +8,15 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table;
-	unsigned long int i;
 
 	new_table = malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
 		return NULL;
 
-	for (i = 0; i < size; i++)
-	{
-		new_table->size = 0;
-		new_table->array = NULL;
-	}
+	new_table->size = size;
+	new_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (new_table->array == NULL)
+		return NULL;
 
 	return (new_table);
 }
