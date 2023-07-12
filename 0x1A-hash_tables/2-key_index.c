a61b791e3f8aa3855863d2382e0854766442c4ce
Olusegun Adeleke <oadeleke123@gmail.com>
2023-07-12 17:43:59 +0000

    fix: betty documentation style

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,7 +1,7 @@
 #include "hash_tables.h"
 
 /**
- * key_index : gives the index of a key
+ * key_index - gives the index of a key
  * @key: the key
  * @size: the size of the array of the hash table
  *
