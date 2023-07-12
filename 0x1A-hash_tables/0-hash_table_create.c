cb713083547fec98f85fda258edf2ae054766b54
Olusegun Adeleke <oadeleke123@gmail.com>
2023-07-12 16:51:17 +0000

    fix: betty

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,12 +11,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	new_table = malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
-		return NULL;
+		return (NULL);
 
 	new_table->size = size;
 	new_table->array = malloc(sizeof(hash_node_t *) * size);
 	if (new_table->array == NULL)
-		return NULL;
+		return (NULL);
 
 	return (new_table);
 }
