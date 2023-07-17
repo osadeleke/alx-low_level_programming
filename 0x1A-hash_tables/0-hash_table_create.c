e4eadbf304b1891f3fba7ac61850b2336a1e3a3f
Olusegun Adeleke <oadeleke123@gmail.com>
2023-07-17 14:45:10 +0000

    fix: delete table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,6 +8,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table;
+	unsigned long int i;
 
 	new_table = malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
@@ -18,5 +19,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (new_table->array == NULL)
 		return (NULL);
 
+	for (i = 0; i < size; i++)
+	{
+		new_table->array[i] = NULL;
+	}
 	return (new_table);
 }
--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -21,13 +21,12 @@ void hash_table_delete(hash_table_t *ht)
 		node = ht->array[i];
 		while (node)
 		{
-			tmp = node;
-			node = node->next;
-			free(tmp->key);
-			free(tmp->value);
-			free(tmp);
+			tmp = node->next;
+			free(node->key);
+			free(node->value);
+			free(node);
+			node = tmp;
 		}
-		free(node);
 	}
 	free(ht->array);
 	free(ht);
